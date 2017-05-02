30bb55e5f12eb270c9ba519442e5cbd42801bfcf
Subramanian <subramanianv@mail.usf.edu>
2017-05-01 20:42:29 -0400

    Update QuantifyGraphAnonymity.py
    Updates to the linkage covariance calculation algorithm.
        Improved for loop
        Improved sorting function using unix sort

--- QuantifyGraphAnonymity.py
+++ QuantifyGraphAnonymity.py
@@ -1,71 +1,44 @@
+from __future__ import division
 import numpy as np
 import pandas as pd
 import logging, sys
+from igraph import *
+import time
+#import matplotlib.pyplot as plt
+from scipy.stats.stats import pearsonr
 
 logging.basicConfig(stream=sys.stderr, level=logging.INFO)
 
-def linkage_covariance(file_path):
-	#Delimiter
-	tabDelimiter = "\t"
-
-	logging.info('Got the file details. Reading file...')
+def linkage_covariance(file_path, order_no):
+	terrorist_igraph = Graph.Read_Ncol(file_path, directed=True)
 	
-	#Use the load text method of numpy with skiprows to skip the header(1st row)
-	d = np.loadtxt(file_path, delimiter="\t", skiprows=1)
-
-	logging.info('File read successfully')
-
-	logging.debug('The input file has been processed for info!\n')
-	logging.debug(d)
-
-	#Use the shape command to get the dimensions of the matrix
 	logging.info('Parsing the graph to get the number of nodes')
-	[m, n] = d.shape
-	header_row = list()
-	logging.info('Creating a header row for the number of nodes')
-	for i in range(0,n):
-		header_row.append(i+1)
-	
-	logging.info('Graph parsed successfully and header row created!')
-	
-	# print the header row
-	logging.debug('Header Row!\n')
-	logging.debug(header_row)
+	n = terrorist_igraph.vcount()
 
-	logging.info('Insert the header row above')
-	#colnames(ax) <- c(1:63)
-	input_graph = d
-	# TO-DO ------ not working!!! - Start
-	#np.insert(input_graph, 1, header_row, axis=0)
-	# TO-DO ------ not working!!! - End
-
-	logging.info('Header row inserted!!')
+	logging.info(n)
 	
-	logging.info('Finding the sum of all the values in each row')
-	#ax.r <- rowSums(ax)
-	input_graph_rowsums = input_graph.sum(axis=1)
-
-	logging.info('Found the sum!')
 	#LC <- matrix(0,n,n)
 	# matrix of zeroes
+	logging.info('Creating a dummy linkage covariance matrix')
 	linkage_covariance_matrix = np.zeros((n, n))
+	logging.info('Collecting the neighbor sets for the linkage covariance matrix')
+	neisets = [set(terrorist_igraph.neighbors(i)) for i in xrange(n)]
+	logging.info('Populating the linkage covariance matrix')
+	for v1, v2 in terrorist_igraph.get_edgelist():
+		common_neis = neisets[v1].intersection(neisets[v2])
+		linkage_covariance_matrix[v1, v2] = (len(common_neis)/n) - (len(neisets[v1]) * len(neisets[v2]))/n**2
+		linkage_covariance_matrix[v2, v1] = linkage_covariance_matrix[v1, v2]
 
-	logging.info('Find the linkage covariance and compute the linkage covariance matrix')
-	
-	#for (i in 1:(n-1)) for (j in (i+1):n) 
-	for i in range(0, n-1):
-		for j in range((i+1), n):
-			#LC[i,j] <- sum(ax[i,]*ax[j,])/n - ax.r[i]*ax.r[j]/n^2 ; LC[j,i] <- LC[i,j]
-			linkage_covariance_matrix[i,j] = sum(input_graph[i,]*input_graph[j,])/n - input_graph_rowsums[i]*input_graph_rowsums[j]/n**2
-			linkage_covariance_matrix[j,i] = linkage_covariance_matrix[i,j]
+	for v1, v2 in np.transpose(np.nonzero(linkage_covariance_matrix == 0)):
+		if v1 != v2:
+			common_neis = neisets[v1].intersection(neisets[v2])
+			linkage_covariance_matrix[v1, v2] = (len(common_neis)/n) - (len(neisets[v1]) * len(neisets[v2]))/n**2
 
 	logging.info('Linkage covariance matrix done')
 	
 	logging.debug('The linkage_covariance_matrix!\n')
 	logging.debug(linkage_covariance_matrix)
-	logging.debug('The input_graph!\n')
-	logging.debug(input_graph)
-
+	
 	# create sorted LC matrix, first sort within each row
 	#RLC.o <- matrix(0,n,n)
 	#rownames(RLC.o) <- c(1:63)
@@ -81,22 +54,15 @@ def linkage_covariance(file_path):
 	logging.debug('RLC!\n')
 	logging.debug(rlc)
 
-	sort_key = list()
-	ascending_key = list()
-	for i in range(0,n):
-		sort_key.append(i)
-		ascending_key.append(False)
-
 	df_rlc = pd.DataFrame(rlc)
 	
 	logging.debug('RLC Data Framed!\n')
 	logging.debug(df_rlc)
 	
-	logging.info('Use a sort key and sort the matrix by each column in ascending order')
+	logging.info('Use a sort key and sort the matrix by each column in descending order')
 	
-	df_rlc.sort_values(sort_key, ascending=ascending_key, inplace=True)
-	rlc = df_rlc.as_matrix()
-	
+	rlc = rlc[rlc[:, 0].argsort()][::-1]
+
 	logging.debug('RLC after making it a matrix with sorted order!\n')
 	logging.debug(rlc)
 
@@ -108,7 +74,7 @@ def linkage_covariance(file_path):
 	#}
 
 	logging.info('Normalize the matrix now!')
-	
+
 	for i in range(0, n):
 		rlc[i,] = rlc[i,]/np.sqrt(np.sum(np.square(rlc[i,])))
 
@@ -116,12 +82,35 @@ def linkage_covariance(file_path):
 	logging.debug(rlc)
 
 	logging.info('Normalized the matrix successfully')
+
+	rlc_pd_df = pd.DataFrame(data=rlc, index=None, columns=None)
 	
-	return rlc
+	return rlc_pd_df
+
+def h_dist(file_path1, file_path2, order):
+	df_rlc_original = linkage_covariance(file_path1, order)
+	print df_rlc_original
+	df_rlc_anon = pd.DataFrame(data=linkage_covariance(file_path2, order), index=df_rlc_original.index.copy(), columns=df_rlc_original.index.copy())
+	df_rlc_anon.fillna(0, inplace=True)
+	print df_rlc_anon
+	pearson_coefficient = np.corrcoef(df_rlc_original[:,0], df_rlc_anon[:, 0])
+	#df_rlc = df_rlc_anon.subtract(df_rlc_original, fill_value=0)
+	#df_rlc = df_rlc.sum(axis=0)
+	#df_rlc = df_rlc.sum()
+	#df_rlc_abs = df_rlc.abs()
+	#print df_rlc
+	print pearson_coefficient
+	#plt.figure()
+	#df_rlc_abs.plot()
 
 def main():
-	print "rankedlinkcharacteristicvectorL2normed: Let us calculate the linkage covariance of the graph"
-	path = raw_input("Path of the graph file?\n")
-	print linkage_covariance(path)
+	print "rankedlinkcharacteristicvectorL2normed_el: Let us calculate the linkage covariance of the graph"
+	original_graph_path = raw_input("Path of the graph file 1?\n")
+	anon_graph_path = raw_input("Path of the graph file 2?\n")
+	order = raw_input("Order of hops?\n")
+	start_time = time.time()
+	h_dist(original_graph_path, anon_graph_path, order)
+	print("--- %s seconds ---" % (time.time() - start_time))
 
 main()
+#plt.show()
